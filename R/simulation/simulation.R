42271c849106cfd634d21b7523bc3e5b5df544f9
leopoldguyot <leopold.guyot13@gmail.com>
2025-04-24 10:16:47 +0200

    modif simulation framework

--- R/simulation/simulation.R
+++ R/simulation/simulation.R
@@ -1,6 +1,9 @@
 library(muscat)
 library(ExperimentHub)
-
+library(dplyr)
+library(scater)
+library(sctransform)
+library(SingleCellExperiment)
 prep_data <- function(data) {
   stopifnot(data %in% c("Kang", "LPS"))
   switch(data,
@@ -53,6 +56,27 @@ simulate_data <- function(prepData) {
                           ns = NULL, nk = NULL,
                           p_dd = diag(6)[1, ], probs = NULL)
     sim
+
+    set.seed(124)
+
+    sim <- simData(prepData,
+                   paired = FALSE, lfc = 2,
+                   ng = nrow(prepData), nc = sim_pars$nc,
+                   ns = sim_pars$ns, nk = sim_pars$nk,
+                   p_dd = sim_pars$p_dd, probs = sim_pars$probs)
+
+    sim <- sim[rowSums(counts(sim) > 0) >= 10, ]
+    sim <- sim[sample(nrow(sim), min(nrow(sim), sim_pars$ng)), ]
+
+    gi <- metadata(sim)$gene_info
+    gi <- dplyr::filter(gi, gene %in% rownames(sim))
+    metadata(sim)$gene_info <- gi
+
+    sim <- computeLibraryFactors(sim)
+    sim <- logNormCounts(sim)
+    assays(sim)$cpm <- calculateCPM(sim)
+    assays(sim)$vstresiduals <- suppressWarnings(
+      vst(counts(sim), show_progress = FALSE)$y)
 }
 
 simulate_testis_data <- function(prepData) {
