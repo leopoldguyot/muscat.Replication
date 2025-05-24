cb5e9ab5df3d2f0a7c2318e97c307455bfe57159
leopoldguyot <leopold.guyot13@gmail.com>
2025-05-24 19:10:51 +0200

    custom pseudobulk function

--- R/pseudobulking.R
+++ R/pseudobulking.R
@@ -15,7 +15,36 @@ aggregate_assay <- function(data, assay, method) {
     ))
     switch(method,
         "None" = data,
-        "Mean" = aggregateData(x = data, fun = "mean"),
-        "Sum" = aggregateData(x = data, fun = "sum")
+        "Mean" = aggregate_wrapper(x = data, fun = "mean"),
+        "Sum" = aggregate_wrapper(x = data, fun = "sum")
     )
+}
+
+
+aggregate_wrapper <- function(x,
+                                assay = NULL,
+                                by = c("cluster_id", "sample_id"),
+                                fun = c("sum", "mean")) {
+    fun <- match.arg(fun)
+        if (is.null(assay)) {
+        assay <- assayNames(x)[1]
+    }
+    for (i in by) {
+        if (!is.factor(x[[i]])) {
+            x[[i]] <- factor(x[[i]])
+        }
+    }
+    mat <- assay(x, assay)
+    groups <- interaction(lapply(by, function(f) x[[f]]), drop = TRUE)
+    agg_fun <- switch(fun,
+                      "sum" = function(m) rowsum(m, group = groups),
+                      "mean" = function(m) rowsum(m, group = groups) / as.vector(table(groups)))
+
+    agg_mat <- agg_fun(t(mat))  # transpose because rowsum aggregates by rows
+    agg_mat <- t(agg_mat)       # transpose back
+
+    new_sce <- SingleCellExperiment(list(agg = agg_mat),
+                                    rowData = rowData(x))
+    colnames(new_sce) <- levels(groups)
+    return(new_sce)
 }
